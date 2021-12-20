a2a5c7e7f6664c34957e5a3488aee81208c2f5aa
Abdurrahman Samgar <smgrjr03@gmail.com>
2021-12-20 17:14:07 +0300

    new entity

--- src/main/java/com/example/newcostumerpro/costumerP/TableContent.java
+++ src/main/java/com/example/newcostumerpro/costumerP/TableContent.java
@@ -1,9 +1,62 @@
 package com.example.newcostumerpro.costumerP;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-
+@Entity
+@Table(name = "NewCostumer")
 public class TableContent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private String value;
+    @Column
+    private String type;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "TableContent{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
