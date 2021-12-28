5c7a25b8a8c8a106c817c33981599c68158dc030
Abdurrahman Samgar <smgrjr03@gmail.com>
2021-12-28 14:20:31 +0300

    new commit

---
+++ src/main/java/com/example/newcostumerpro/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.example.newcostumerpro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+    @GetMapping("/admin")
+    public String adminHome(){
+        return "adminHome";
+    }
+
+    @GetMapping("admin/categories")
+    public String getCat(){
+        return "categories";
+
+    }
+}
